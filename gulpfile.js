884655dc753487704a9177bad9c7ebb9b8004407
Naoki Endoh <n-endo@puzzle-inc.jp>
2014-03-10 18:25:48 +0900

    Creating tasks: Compile sass, autoprefix and minify

--- gulpfile.js
+++ gulpfile.js
@@ -12,3 +12,15 @@ var gulp = require('gulp'),
     livereload = require('gulp-livereload'),
     lr = require('tiny-lr'),
     server = lr();
+
+gulp.task('styles', function() {
+    return gulp.src('src/styles/main.scss')
+        .pipe(sass({ style: 'expanded' }))
+        .pipe(autoprefixer('last 2 version', 'safari 5', 'ie 8', 'ie 9', 'opera 12.1', 'ios 6', 'android 4'))
+        .pipe(gulp.dest('dist/assets/css'))
+        .pipe(rename({ suffix: '.min' }))
+        .pipe(minifycss())
+        .pipe(gulp.dest('dist/assets/css'))
+        .pipe(livereload(server))
+        .pipe(notify({ message: 'Styles task complete' }));
+});
